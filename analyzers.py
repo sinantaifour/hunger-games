74a8d62142d2232ae4651b414b25425f1805e80f
Sinan Taifour <sinan@taifour.com>
2013-08-16 11:33:15 +0300

    Abstracted some of the analyzer code into BaseAnalyzer.

--- analyzers.py
+++ analyzers.py
@@ -4,13 +4,28 @@ import matplotlib.pyplot
 matplotlib.pyplot.ion()
 from pylab import *
 
-class FoodAnalyzer():
+class BaseAnalyzer():
   def __init__(self, game):
     self.game = game
+  def dataset(self):
+    pass
   def plot(self, meta = False):
     figure()
-    plot(self.game.history['food'])
+    plot(self.dataset())
     if meta:
       legend([p.name for p in self.game.unshuffled])
     else:
       legend(['' for p in self.game.unshuffled])
+
+class FoodAnalyzer(BaseAnalyzer):
+  def dataset(self):
+    return self.game.history['food']
+
+class RepAnalyzer(BaseAnalyzer):
+  def dataset(self):
+    res = []
+    for i in range(1, len(self.game.history['payouts']) + 1):
+      res.append([self.rep(h, s) for (h, s) in zip(self.game.history['hunts'][i], self.game.history['slacks'][i])])
+    return res
+  def rep(self, h, s):
+    return float(h)/(h+s) if h > 0 else 0
