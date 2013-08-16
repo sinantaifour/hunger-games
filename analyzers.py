a856ea155c2f736c5a9de86fa75ba1b15583676d
Sinan Taifour <sinan@taifour.com>
2013-08-16 11:51:03 +0300

    Analyzers can save figures, and use dotted and dashed lines.

--- analyzers.py
+++ analyzers.py
@@ -3,23 +3,46 @@ matplotlib.use('tkagg')
 import matplotlib.pyplot
 matplotlib.pyplot.ion()
 from pylab import *
+from itertools import product
 
 class BaseAnalyzer():
   def __init__(self, game):
     self.game = game
-  def dataset(self):
+  def dataset(self): # Dataset, to be overriden by the subclasses.
     pass
-  def plot(self, meta = False):
-    figure()
-    plot(self.dataset())
+  def fname(self): # Default filename, to be overridden by the subclasses.
+    pass
+  def loc(self): # Location of legend, optionally to be overriden by the subclasses.
+    return 'lower right'
+  def plot(self, meta = True):
+    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+    dashes = ['', '--', ':']
+    combinations = [x for x in product(dashes, colors)]
+    self.figure = figure()
+    lines = plot(self.dataset())
+    for i, line in enumerate(lines):
+      dash, color = combinations[i]
+      line.set_color(color)
+      if dash:
+        line.set_ls(dash)
     if meta:
-      legend([p.name for p in self.game.unshuffled])
-    else:
-      legend(['' for p in self.game.unshuffled])
+      legend([p.name for p in self.game.unshuffled], loc=self.loc())
+  def save(self, fname = None):
+    if fname == None:
+      fname = self.fname()
+    figure(self.figure.number)
+    savefig(fname)
 
 class FoodAnalyzer(BaseAnalyzer):
   def dataset(self):
     return self.game.history['food']
+  def fname(self):
+    return "food.png"
+  def loc(self):
+    if max(self.game.history['food'][-1]) > max(self.game.history['food'][0]):
+      return 'upper left'
+    else:
+      return 'lower left'
 
 class RepAnalyzer(BaseAnalyzer):
   def dataset(self):
@@ -27,5 +50,7 @@ class RepAnalyzer(BaseAnalyzer):
     for i in range(1, len(self.game.history['payouts']) + 1):
       res.append([self.rep(h, s) for (h, s) in zip(self.game.history['hunts'][i], self.game.history['slacks'][i])])
     return res
+  def fname(self):
+    return "rep.png"
   def rep(self, h, s):
     return float(h)/(h+s) if h > 0 else 0
