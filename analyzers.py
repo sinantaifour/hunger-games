2c4719a6b6146889ccd017f91e41199327e295b1
Sinan Taifour <sinan@taifour.com>
2013-08-16 12:04:10 +0300

    Adding blank lines to satisfy the Ramy.

--- analyzers.py
+++ analyzers.py
@@ -6,14 +6,19 @@ from pylab import *
 from itertools import product
 
 class BaseAnalyzer():
+
   def __init__(self, game):
     self.game = game
+
   def dataset(self): # Dataset, to be overriden by the subclasses.
     pass
+
   def fname(self): # Default filename, to be overridden by the subclasses.
     pass
+
   def loc(self): # Location of legend, optionally to be overriden by the subclasses.
     return 'lower right'
+
   def plot(self, meta = True):
     colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
     dashes = ['', '--', ':']
@@ -27,30 +32,39 @@ class BaseAnalyzer():
         line.set_ls(dash)
     if meta:
       legend([p.name for p in self.game.unshuffled], loc=self.loc())
+
   def save(self, fname = None):
     if fname == None:
       fname = self.fname()
     figure(self.figure.number)
     savefig(fname)
 
+
 class FoodAnalyzer(BaseAnalyzer):
+
   def dataset(self):
     return self.game.history['food']
+
   def fname(self):
     return "food.png"
+
   def loc(self):
     if max(self.game.history['food'][-1]) > max(self.game.history['food'][0]):
       return 'upper left'
     else:
       return 'lower left'
 
+
 class RepAnalyzer(BaseAnalyzer):
+
   def dataset(self):
     res = []
     for i in range(1, len(self.game.history['payouts']) + 1):
       res.append([self.rep(h, s) for (h, s) in zip(self.game.history['hunts'][i], self.game.history['slacks'][i])])
     return res
+
   def fname(self):
     return "rep.png"
+
   def rep(self, h, s):
     return float(h)/(h+s) if h > 0 else 0
